be4b7e2b42550a932347219e556cc21fde53354b
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-09 18:06:49 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,15 +20,18 @@ def third_challenge
       :patriarch => {
         :name => "Lord Capulet",
         :age => "50"
+      },
       :matriarch => {
         :name => "Lady Capulet", 
         :age => "51", 
+      },
       :heroine => {
         :name => "Juliet", 
         :age => "15", 
-        :status => "alive" },
+        :status => "alive" 
+      },
       :heroine_friends => []
-   }
+      }
   }
 
   
